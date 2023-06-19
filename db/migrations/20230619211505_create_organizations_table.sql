a78fb93c65b0f3c3fe4f5edb92a73fc69e37eb31
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2023-06-19 16:25:53 -0500

    [CORE] Update migrations

--- db/migrations/20230619211505_create_organizations_table.sql
+++ db/migrations/20230619211505_create_organizations_table.sql
@@ -2,11 +2,11 @@
 
 CREATE TABLE IF NOT EXISTS core.organizations (
   organization_id uuid NOT NULL,
-  organization_name VARCHAR(127) NOT NULL;
-  organization_client_id VARCHAR(63) NOT NULL;
-  organization_alpha_user int4 NULL;
-  organization_created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP(6);
-  organization_updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP(6);
+  organization_name VARCHAR(127) NOT NULL,
+  organization_client_id VARCHAR(63) NOT NULL,
+  organization_alpha_user int4 NULL,
+  organization_created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+  organization_updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP(6)
 );
 
 -- migrate:down
--- db/migrations/20230619212503_create_users_table.sql
+++ db/migrations/20230619212503_create_users_table.sql
@@ -1,5 +1,9 @@
 -- migrate:up
 
+CREATE TYPE core.role_enum AS ENUM ('INTERNAL_ADMIN', 'EXTERNAL_ADMIN', 'USER');
+CREATE TYPE core.auth_provider_enum AS ENUM ('FIREBASE', 'SUPABASE');
+CREATE TYPE core.auth_type_enum AS ENUM ('EMAIL_AND_PASSWORD', 'FACEBOOK_AUTH', 'GOOGLE_AUTH', 'GITHUB_AUTH');
+
 CREATE TABLE IF NOT EXISTS core.users (
   user_id uuid NOT NULL,
   user_username VARCHAR(63) NOT NULL,
@@ -10,8 +14,9 @@ CREATE TABLE IF NOT EXISTS core.users (
   user_notifications bool NOT NULL DEFAULT false,
   user_is_active bool NOT NULL DEFAULT true,
   user_uid VARCHAR(255) NOT NULL,
-  user_role syk."enum_auth_provider" NOT NULL,
-  user_provider syk."users_user_provider_enum" NOT NULL,
+  user_role core."role_enum" NOT NULL,
+  user_provider core."auth_provider_enum" NOT NULL,
+  user_auth_type core."auth_type_enum" NOT NULL,
   user_organization int4 NOT NULL,
   user_created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
   user_updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
