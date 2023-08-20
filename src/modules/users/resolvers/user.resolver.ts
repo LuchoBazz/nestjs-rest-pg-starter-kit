014f687412c1042c10ea66a918a2dfb84b08604c
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2023-08-20 11:44:15 -0500

    [FEATURE] Add Get Client Ip decorator (#105)

--- src/modules/users/resolvers/user.resolver.ts
+++ src/modules/users/resolvers/user.resolver.ts
@@ -3,6 +3,7 @@ import { Query, Resolver } from '@nestjs/graphql';
 
 import { UserEntity, UserObject } from '../../../entities/users';
 import { JwtUser } from '../../authentication/decorators';
+import { GetClientIP } from '../../authentication/decorators/client-ip.decorator';
 import { JwtAuthGuard } from '../../authentication/guards';
 import { UserInteractor } from '../interactors/user.interactor';
 
@@ -12,7 +13,8 @@ export class UserResolver {
 
   @UseGuards(JwtAuthGuard)
   @Query(() => UserObject, { nullable: false })
-  public currentUser(@JwtUser() user: UserEntity): Promise<UserObject> {
+  public currentUser(@JwtUser() user: UserEntity, @GetClientIP() ip: string | null): Promise<UserObject> {
+    console.log(ip);
     return this.userInteractor.findOne({ clientId: user.organization_client_id, email: user.email });
   }
 }
