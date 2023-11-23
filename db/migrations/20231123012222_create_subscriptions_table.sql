d4eb97d6d0e995cce8fdfd0cb09f47ce9d31ef8e
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2023-11-22 20:22:48 -0500

    Add Create Subscriptions table

---
+++ db/migrations/20231123012222_create_subscriptions_table.sql
@@ -0,0 +1,19 @@
+-- migrate:up
+
+CREATE TABLE core.subscriptions (
+  subscriptions_id                    UUID NOT NULL,
+  subscriptions_user_id               UUID NOT NULL UNIQUE,
+  subscriptions_subscription_plan_id  UUID NOT NULL,
+  subscriptions_frequency             VARCHAR(63) NOT NULL,
+  subscriptions_is_active             BOOLEAN NOT NULL DEFAULT TRUE,
+  subscriptions_renews_at             TIMESTAMPTZ,
+  subscriptions_starts_at             TIMESTAMPTZ DEFAULT NOW(),
+  subscriptions_ends_at               TIMESTAMPTZ,
+  subscriptions_created_at            TIMESTAMPTZ DEFAULT NOW(),
+  subscriptions_updated_at            TIMESTAMPTZ DEFAULT NOW(),
+  CONSTRAINT subscriptions_pk PRIMARY KEY (subscriptions_id),
+  CONSTRAINT subscriptions_user_id_fk FOREIGN KEY (subscriptions_user_id) REFERENCES core.users(user_id),
+  CONSTRAINT subscriptions_subscription_plan_id_fk FOREIGN KEY (subscriptions_subscription_plan_id) REFERENCES core.subscription_plans(subscription_plan_id)
+);
+
+-- migrate:down
