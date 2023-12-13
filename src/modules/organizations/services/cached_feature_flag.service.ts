53e290e58c7f859d6858b0cd593a49807023bcd1
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2023-12-13 10:42:15 -0500

    refactor: Add findOne cachedFeatureFlag

--- src/modules/organizations/services/cached_feature_flag.service.ts
+++ src/modules/organizations/services/cached_feature_flag.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { PoolClient } from 'pg';
 
 import { CacheService } from '../../../common/cache/cache.service';
+import { OrganizationCacheParameters } from '../../../entities/cache';
+import { FeatureFlagEntity } from '../../../entities/organizations';
 import { AuthProvider } from '../../../entities/users/user.entity';
 import { FeatureFlagRepository } from '../repositories/feature_flag.repository';
 
@@ -9,14 +11,14 @@ import { FeatureFlagRepository } from '../repositories/feature_flag.repository';
 export class CachedFeatureFlagService {
   constructor(
     private readonly cacheService: CacheService,
-    private readonly featureFlagRepository: FeatureFlagRepository,
+    private readonly featFlagRepository: FeatureFlagRepository,
   ) {}
 
   public async findAuthProvider(manager: PoolClient, { clientId }: { clientId: string }): Promise<AuthProvider> {
     /*const parameter = new OrganizationCacheParameters(clientId, FeatureFlagKey.AUTH_PROVIDER);
     try {
       const searcher = (manager: PoolClient, params: string[]): Promise<FeatureFlagEntity | null> => {
-        return this.featureFlagRepository.findOne(manager, { clientId: params[0], key: params[1] });
+        return this.featFlagRepository.findOne(manager, { clientId: params[0], key: params[1] });
       };
       const flag = await this.cacheService.get(parameter, manager, searcher);
       const parsed = parseEnum(AuthProvider, flag.value);
@@ -27,5 +29,16 @@ export class CachedFeatureFlagService {
     }*/
     console.log({ manager, clientId });
     return AuthProvider.FIREBASE;
+  }
+
+  public async findOne(
+    manager: PoolClient,
+    { clientId, key }: { clientId: string; key: string },
+  ): Promise<FeatureFlagEntity | null> {
+    const parameter = new OrganizationCacheParameters(clientId, key);
+    const searcher = async (manager: PoolClient, params: string[]): Promise<FeatureFlagEntity | null> => {
+      return this.featFlagRepository.findOne(manager, { clientId: params[0], key: params[1] });
+    };
+    return this.cacheService.get(parameter, manager, searcher);
   }
 }
